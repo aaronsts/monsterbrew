ec949a8df899000904b1a4ea5d3ac355411fe89d
Aaron <aaron.staes@gmail.com>
2025-03-23 08:18:02 +0100

    chore: add notice for V2

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,24 +1,44 @@
 import ClientEditor from "@/components/editor/client-editor";
 import { getAllCreatures } from "@/services/creatures";
 import {
-	HydrationBoundary,
-	QueryClient,
-	dehydrate,
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
 } from "@tanstack/react-query";
+import { Info } from "lucide-react";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Editor() {
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	await queryClient.prefetchQuery({
-		queryKey: ["creatures"],
-		queryFn: getAllCreatures,
-	});
-	return (
-		<Suspense>
-			<HydrationBoundary state={dehydrate(queryClient)}>
-				<ClientEditor />
-			</HydrationBoundary>
-		</Suspense>
-	);
+  await queryClient.prefetchQuery({
+    queryKey: ["creatures"],
+    queryFn: getAllCreatures,
+  });
+  return (
+    <Suspense>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="bg-cararra-100 border border-cararra-800 px-4 py-3 rounded-md flex gap-2">
+          <Info />
+          <div>
+            <h3>Monsterbrew V2</h3>
+            <p className="leading-snug">
+              I have been working on the next version of monsterbrew for quite a
+              while now, it will soon be released and replace this one. <br />{" "}
+              You can already test it out here:{" "}
+              <Link
+                className="font-semibold hover:underline underline-offset-2"
+                href="https://monsterbrew-app.vercel.app/"
+                target="_blank"
+              >
+                Visit Monsterbrew V2
+              </Link>
+            </p>
+          </div>
+        </div>
+        <ClientEditor />
+      </HydrationBoundary>
+    </Suspense>
+  );
 }
--- src/components/editor/client-editor.tsx
+++ src/components/editor/client-editor.tsx
@@ -14,46 +14,46 @@ import { useSearchParams } from "next/navigation";
 import LoadingSpinner from "../ui/loading-spinner";
 
 export default function ClientEditor() {
-	const searchParams = useSearchParams();
-	const { setCreatures, setSelectedCreature, selectedCreature } =
-		useCreaturesStore();
-	const { form, loadCreatureValues, onSubmit } = useCreatureForm();
+  const searchParams = useSearchParams();
+  const { setCreatures, setSelectedCreature, selectedCreature } =
+    useCreaturesStore();
+  const { form, loadCreatureValues, onSubmit } = useCreatureForm();
 
-	const { data } = useQuery({
-		queryKey: ["creatures"],
-		queryFn: getAllCreatures,
-	});
+  const { data } = useQuery({
+    queryKey: ["creatures"],
+    queryFn: getAllCreatures,
+  });
 
-	useEffect(() => {
-		if (!data) return;
-		setCreatures(data.results);
-	}, [data, setCreatures]);
+  useEffect(() => {
+    if (!data) return;
+    setCreatures(data.results);
+  }, [data, setCreatures]);
 
-	useEffect(() => {
-		const paramsCreature = searchParams.get("creature");
-		if (paramsCreature === null) return;
-		setSelectedCreature(paramsCreature as string);
-	}, [searchParams, setSelectedCreature]);
+  useEffect(() => {
+    const paramsCreature = searchParams.get("creature");
+    if (paramsCreature === null) return;
+    setSelectedCreature(paramsCreature as string);
+  }, [searchParams, setSelectedCreature]);
 
-	const { isLoading } = useQuery({
-		queryKey: ["creature", selectedCreature],
-		queryFn: () => getCreature(selectedCreature),
-	});
+  const { isLoading } = useQuery({
+    queryKey: ["creature", selectedCreature],
+    queryFn: () => getCreature(selectedCreature),
+  });
 
-	return (
-		<div className="grid md:grid-cols-2 gap-6">
-			<StatblockForm form={form} onSubmit={onSubmit} />
-			<div className="flex flex-col relative gap-3">
-				<div className="print:hidden flex items-center justify-end sticky top-16 bg-white py-3 gap-3">
-					<ImportButton />
-					<ExportOptions />
-				</div>
-				{isLoading ? (
-					<LoadingSpinner />
-				) : (
-					<Statblock loadCreatureValues={loadCreatureValues} />
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="grid md:grid-cols-2 gap-6">
+      <StatblockForm form={form} onSubmit={onSubmit} />
+      <div className="flex flex-col relative gap-3">
+        <div className="print:hidden flex items-center justify-end sticky top-16 bg-white py-3 gap-3">
+          <ImportButton />
+          <ExportOptions />
+        </div>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <Statblock loadCreatureValues={loadCreatureValues} />
+        )}
+      </div>
+    </div>
+  );
 }
